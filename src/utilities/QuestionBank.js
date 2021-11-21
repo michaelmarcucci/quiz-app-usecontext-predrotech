0c3e10ef50295744fe03eea57f1dc477146e6acc
michaelmarcucci <64811901+michaelmarcucci@users.noreply.github.com>
2021-11-21 00:21:22 -0500

    practice usecontext react

---
+++ src/utilities/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+        prompt: "What is 1 + 3?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C"
+    },
+    {
+        prompt: "Which is a streaming platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robin",
+        answer: "B"
+    },
+    {
+        prompt: "Best tech youtube channel?",
+        optionA: "PedroTech",
+        optionB: "Fireship",
+        optionC: "DevEd",
+        optionD: "TechwithTim",
+        answer: "A"
+    },
+]
